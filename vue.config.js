985c9c613b9bd9d910f9bf3038b47f0a8816e920
Zaiyiiiiiiiiii <wangzaiyiw@gmail.com>
2019-11-20 10:31:59 +0000

    add vue settings

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,14 @@ module.exports = {
     devServer: {
         proxy: {
             "/api/netease": {
-                target: "http://localhost:3000/",  
+                target: "http://localhost:3000/",
                 // ignorePath: true,
-                pathRewrite: {'^/api/netease' : ''},
+                pathRewrite: { '^/api/netease': '' },
                 changeOrigin: true
             }
-        }        
+        },
+        disableHostCheck: true,
+        host: '0.0.0.0',
+        port: 8080
     }
 }
